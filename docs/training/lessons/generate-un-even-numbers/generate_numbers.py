8023a1ce4a32a663d6fd23f55273900626976754
Holger Joukl <holger.joukl@lbbw.de>
2023-09-13 13:46:46 +0200

    Revamp gen. even/uneven numbers lesson solution

--- docs/training/lessons/generate-un-even-numbers/generate_numbers.py
+++ docs/training/lessons/generate-un-even-numbers/generate_numbers.py
@@ -1,48 +1,129 @@
-def even(n):
-    print('generate even-numbers')
-    for i in range(n):
-        if i%2 == 0: yield i
-
-def uneven(n):
-    print('generate uneven-numbers')
-    for i in range(n):
-        if i%2 != 0: yield i
-
-def main(n):
-    ### 1. Using list compreheions
-    # NOTE: Here we just get and print generator object
-    #print('<<< 0 >>>')
-    #[print(f'even: {number for number in range(n) if number%2 == 0}')]
-    #[print(f'uneven: {number for number in range(n) if number%2 != 0}')]
-
-    # NOTE: Here get and print the whole list at once
-    print('<<< 1 >>>')
-    print([f'even: {number}' for number in range(n) if number%2 == 0])
-    print([f'uneven: {number}' for number in range(n) if number%2 != 0])
-
-    # NOTE: Here get the list and print each individual element
-    print('<<< 2 >>>')
-    [print(f'even: {number}') for number in range(n) if number%2 == 0]
-    [print(f'uneven: {number}') for number in range(n) if number%2 != 0]
-  
-    # NOTE: Here get the list and print each individual element - but as 2 steps
-    print('<<< 3 >>>')
-    _even = [number for number in range(n) if number%2 == 0]
-    for i in _even: print(f'even: {i}')
-    _uneven = [number for number in range(n) if number%2 != 0]
-    for i in _uneven: print(f'uneven: {i}')
+def gen_even():
+    """Generate even natural numbers, undefinitely.
+    """
+    i = 1
+    while True:
+        if i % 2 == 0:
+            yield i
+        i += 1
 
 
-    ### 2. Using generator-functions
-    # call even-generator function
-    print('<<< 4 >>>')
-    for i in even(n):
-        print(f'even: {i}')
+def gen_uneven():
+    """Generate even natural numbers, undefinitely.
+    """
+    i = 1
+    while True:
+        if i % 2 != 0:
+            yield i
+        i += 1
 
-    # call uneven-generator function
-    for i in uneven(n):
-        print(f'uneven: {i}')
-    
 
-if __name__ == '__main__':
-    main(n=10)
+def gen_even_lim(upper_limit=-1):
+    """Generate even natural numbers, up to max upper limit, or undefinitely if
+    max < 1.
+    """
+    for i in gen_even():
+        if i > upper_limit:
+            break
+        yield i
+
+
+def gen_uneven_lim(upper_limit=-1):
+    """Generate uneven natural numbers, up to max upper limit, or undefinitely
+    if max < 1.
+    """
+    for i in gen_uneven():
+        if i > upper_limit:
+            break
+        yield i
+
+
+def gen_numbers(mode="even"):
+    """Generate even or uneven natural numbers, undefinitely.
+    """
+    if mode == "even":
+        return gen_even()
+    elif mode == "uneven":
+        return gen_uneven()
+    else:
+        raise ValueError(f"mode '{mode}' is not supported, use {even, uneven}")
+
+
+def gen_numbers_lim(mode="even", upper_limit=-1):
+    """Generate even or uneven natural numbers up to max upper limit, or
+    undefinitely if max < 1.
+    """
+    if mode == "even":
+        return gen_even_lim(upper_limit)
+    elif mode == "uneven":
+        return gen_uneven_lim(upper_limit)
+    else:
+        raise ValueError(f"mode '{mode}' is not supported, use {even, uneven}")
+
+
+def main():
+    # Run unlimited generator, but break out after a limit (checked outside of
+    # the generator). In the REPL you'd interrupt the generator using CTRL-C.
+    generator = gen_even
+    print()
+    print(f"*** Generator: {generator.__name__}")
+    print(f"({generator.__doc__.strip()})")
+    for val in generator():
+        if val > 10:
+            print("...interrupting (use CTRL-C when running in REPL)")
+            break
+        print(f"even: {val}")
+
+    # Run even/uneven-switchable unlimited generator in "even" mode, but break
+    # out after a limit (checked outside of the generator). In the REPL you'd
+    # interrupt the generator using CTRL-C.
+    generator = gen_numbers
+    print()
+    mode = "even"
+    print(f"*** Generator: {generator.__name__}(mode='{mode}')")
+    print(f"({generator.__doc__.strip()})")
+    for val in generator(mode):
+        if val > 10:
+            print("...interrupting (use CTRL-C when running in REPL)")
+            break
+        print(f"even: {val}")
+
+    # Run even/uneven-switchable unlimited generator in "even" mode, but break
+    # out after a limit.
+    print()
+    mode = "uneven"
+    print(f"*** Generator: {generator.__name__}(mode='{mode}')")
+    print(f"({generator.__doc__.strip()})")
+    for val in generator(mode):
+        if val > 10:
+            print("...interrupting (use CTRL-C when running in REPL)")
+            break
+        print(f"uneven: {val}")
+
+
+    # Run even/uneven-switchable limited generator in "even" mode with an upper
+    # limit of 5.
+    generator = gen_numbers_lim
+    print()
+    mode = "even"
+    limit = 5
+    print(f"*** Generator: {generator.__name__}(mode='{mode}', limit={limit})")
+    print(f"({generator.__doc__.strip()})")
+    for val in generator(mode=mode, upper_limit=limit):
+        print(f"even: {val}")
+
+    # Run even/uneven-switchable limited generator in "uneven" mode with an
+    # upper limit of 5.
+    print()
+    mode = "uneven"
+    limit = 5
+    print(f"*** Generator: {generator.__name__}(mode='{mode}', limit={limit})")
+    print(f"({generator.__doc__.strip()})")
+    for val in generator(mode=mode, upper_limit=limit):
+        print(f"uneven: {val}")
+
+
+if __name__ == "__main__":
+    # If this program is invoked as the main program, as opposed to being
+    # imported, run the main function.
+    main()
